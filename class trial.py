c21381bc9d28cc7f399d4ad014830171df9d15ad
Mahmoud819 <67577538+Mahmoud819@users.noreply.github.com>
2022-09-13 19:55:08 -0400

    Add files via upload

---
+++ class trial.py
@@ -0,0 +1,29 @@
+print ( 'eneter the values of x and y as integers:')
+x= int(input())
+y= int(input ())
+z = x + y
+print (z)
+
+print ( 'eneter the values of l and m as integers:')
+l= int(input())
+m= int(input ())
+k = l * m
+print (k)
+
+print ( 'eneter the values of o and p as integers:')
+o= int(input())
+p= int(input ())
+q = o ** p
+print (q)
+
+print ( 'eneter the values of a and b as integers:')
+a= int(input())
+b= int(input ())
+c = b / a
+print (c)
+
+print ( 'eneter the values of r and s as integers:')
+r= int(input())
+s= int(input ())
+t = r - s
+print (s)
