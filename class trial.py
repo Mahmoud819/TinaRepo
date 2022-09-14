8e2b2863f0704f5ce53220bf8f8c9614947ce55e
Tina Zavvar <104872938+TinaZavvar@users.noreply.github.com>
2022-09-13 20:11:54 -0400

    Update class trial.py

--- class trial.py
+++ class trial.py
@@ -1,29 +1,35 @@
-print ( 'eneter the values of x and y as integers:')
+print ( 'eneter the values of x and y and y2 as integers:')
 x= int(input())
 y= int(input ())
-z = x + y
+y2= int(input ()) #Tina
+
+z=x+y+y2  #Edited by Tina
 print (z)
 
-print ( 'eneter the values of l and m as integers:')
+print ( 'eneter the values of l and m and m2 as integers:')
 l= int(input())
 m= int(input ())
-k = l * m
+m2= int(input ()) #Edited by Tina
+k = l * m* m2 #Edited by Tina
 print (k)
 
-print ( 'eneter the values of o and p as integers:')
+print ( 'eneter the values of o and p and p2 as integers:')
 o= int(input())
 p= int(input ())
-q = o ** p
+p2=int(input ()) #Edited by Tina
+q = o ** p ** p2 #Edited by Tina
 print (q)
 
-print ( 'eneter the values of a and b as integers:')
+print ( 'eneter the values of a and b and b2 as integers:')
 a= int(input())
 b= int(input ())
-c = b / a
+b2=int(input ()) #Edited by Tina
+c = b / a/ b2 #Edited by Tina
 print (c)
 
-print ( 'eneter the values of r and s as integers:')
+print ( 'eneter the values of r and s and s2 as integers:')
 r= int(input())
 s= int(input ())
-t = r - s
-print (s)
+s2= int(input ()) #Edited by Tina
+t = r - s -s2 #Edited by Tina
+print (t)
